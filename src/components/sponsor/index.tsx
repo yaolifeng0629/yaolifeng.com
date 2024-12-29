1a06b3628a205dda91531a5b7ca06ade3dfbb6f8
Immerse <y15829485647@163.com>
2024-12-29 16:28:27 +0800

    feat: add sponsor enter (#43)

---
+++ src/components/sponsor/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+type Props = {
+    className?: string;
+};
+
+const Sponsor = ({ className }: Props) => {
+    return (
+        <Link
+            href="https://ims-oss.us.kg/html/simple.html"
+            target='_blank'
+            className="text-muted-foreground hover:text-foreground"
+            title='Sponsor'
+        >
+            <Image
+                src="/other/like.svg"
+                alt="Sponsor me"
+                width={40}
+                height={40}
+                className="h-5 w-5"
+            />
+        </Link>
+    );
+};
+
+export default Sponsor;
