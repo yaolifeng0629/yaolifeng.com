fc2ed3929d91217f587964cf87bd24e40525dab4
Immerse <y15829485647@163.com>
2025-03-10 14:29:45 +0800

    perf: opt seo share img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export const metadata: Metadata = {
     title: '姚利锋',
     description:
         '欢迎来到姚利锋的个人网站 yaolifeng.com，这里分享我的前端开发、技术思考与学习心得。我热爱开源 (Github: github.com/yaolifeng0629)，并探索各种实用工具，我的独立开发工具站 www.indietools.work。我也热衷远程工作，欢迎关注微信公众号【沉浸式趣谈】',
-    keywords: ['沉浸式趣谈', '开源', '沉浸式', 'yaolifeng0629', 'Immerse', 'INDIE TOOLS'],
+    keywords: ['沉浸式趣谈', '开源', '沉浸式', 'yaolifeng0629', 'Immerse', 'INDIE TOOLS', '姚利锋'],
+    generator: 'Next.js',
+    applicationName: '姚利锋',
     metadataBase: new URL('https://yaolifeng.com'),
     alternates: {
         canonical: '/',
@@ -35,20 +37,34 @@ export const metadata: Metadata = {
         type: 'website',
         images: [
             {
-                url: '/images/Immerse-dark.svg',
-                width: 1200,
-                height: 630,
-                alt: '姚利锋',
+                url: '/other/share/share.png',
+                width: 1920,
+                height: 1080,
+                alt: 'yaolifeng.com',
             },
         ],
     },
     twitter: {
         card: 'summary_large_image',
-        site: '@Immerse_code',
+        site: 'https://yaolifeng.com',
+        creator: '@Immerse_code',
         title: '姚利锋',
         description:
             '欢迎来到姚利锋的个人网站 yaolifeng.com，这里分享我的前端开发、技术思考与学习心得。我热爱开源 (Github: github.com/yaolifeng0629)，并探索各种实用工具，我的独立开发工具站 www.indietools.work。我也热衷远程工作，欢迎关注微信公众号【沉浸式趣谈】',
-        images: ['/images/Immerse-dark.svg'],
+        images: [
+            {
+                url: '/other/share/share.png',
+                width: 1920,
+                height: 1080,
+                alt: 'yaolifeng.com',
+            },
+        ],
+    },
+    viewport: {
+        width: 'device-width',
+        initialScale: 1,
+        maximumScale: 1,
+        userScalable: false,
     },
     robots: {
         index: true,
@@ -65,9 +81,19 @@ export const metadata: Metadata = {
         'X-Robots-Tag': 'index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1',
     },
     icons: {
-        icon: '/images/Immerse-dark.svg',
-        shortcut: '/images/Immerse-dark.svg',
+        icon: [
+            { url: '/images/Immerse-dark.svg', sizes: 'any' },
+            { url: '/images/Immerse-dark.svg', type: 'image/svg+xml' },
+        ],
+        apple: '/images/Immerse-dark.svg', // iOS设备
+        shortcut: '/images/Immerse-dark.svg', // 快捷方式图标
+        other: {
+            rel: 'mask-icon', // Safari固定标签页
+            url: '/images/Immerse-dark.svg',
+            color: '#000000',
+        },
     },
+    manifest: '/manifest.json',
 };
 
 export default function RootLayout({
